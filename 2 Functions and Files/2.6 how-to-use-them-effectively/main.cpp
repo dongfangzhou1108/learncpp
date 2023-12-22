532b8da54f3bde1f3be6ae6af2ddcd67c929018f
dongfangzhou1108 <18746061711@163.com>
2023-12-22 16:35:15 +0800

    什么情况下定义函数

---
+++ 2 Functions and Files/2.6 how-to-use-them-effectively/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: dongfangzhou1108 18746061711@163.com
+ * @Date: 2023-12-22 15:57:02
+ * @LastEditors: dongfangzhou1108 18746061711@163.com
+ * @LastEditTime: 2023-12-22 16:34:59
+ * @FilePath: /learncpp/2 Functions and Files/2.6
+ * how-to-use-them-effectively/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
+ * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+// (1) 在程序中多次出现的语句组通常应转换为一个函数
+// (2) Code that has a well-defined set of inputs and outputs is a good
+// candidate for a function A function should generally perform one (and only
+// one) task
+
+int main() {}
